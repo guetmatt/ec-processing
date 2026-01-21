03cca6faaf588ed1dcfdd6391131db033b29f09a
guetmatt <mguetebier@uni-osnabrueck.de>
2026-01-21 19:10:36 +0100

    new inference pipeline script --first working version

--- dev_pipeline_inference.py
+++ dev_pipeline_inference.py
@@ -14,35 +14,35 @@ import json
 import torch
 import itertools
 from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
-
+import pandas as pd
 
 # %%
 # functions
 def run_ner_module(text, ner_pipe):
     """
     Applies NER model to text.
-    Returns list of entities and saves them to disk.
+    Returns list of predicted entities.
     """
     # apply NER
-    entities = ner_pipe(text)
+    predictions = ner_pipe(text)
     
     # maybe --> filter low confidence entities
     # entities = [ent for ent in entities if ent['score'] > 0.4
     
-    # save results
-    ner_output_path = os.path.join(OUTPUT_DIR, "ner_output.json")
-    with open(ner_output_path, "w", encoding="UTF-8") as f:
-        serializable_entities = list()
-        for ent in entities:
-            ent_clean = ent.copy()
-            ent_clean["score"] = float(ent["score"])
-            ent_clean["start"] = int(ent["start"])
-            ent_clean["end"] = int(ent["end"])
-            serializable_entities.append(ent_clean)
-        json.dump(serializable_entities, f, indent=4)
-    print(f"NER results saved to {ner_output_path}")
-    
-    return serializable_entities
+    # format predictions for json serialization
+    predicted_entities = list()
+    for pred in predictions:
+        clean_pred = {
+            "entity_group": pred["entity_group"],
+            "word": pred["word"],
+            "start": int(pred["start"]),
+            "end": int(pred["end"]),
+            "score": float(pred["score"])
+        }
+        predicted_entities.append(clean_pred)
+        
+    return predicted_entities
+
 
 
 
@@ -132,10 +132,31 @@ def run_re_module(text, entities, re_tokenizer, re_model):
     
     
     
+# %%
+if __name__ == "__main__":
     
-
-def main():
-    # (1) load NER model and tokenizer
+    
+    # %%
+    # --- (0) configurations ---
+    DATA_DIR = "./data_by_lea/data_parsed_sentLevel/ctg_sentLevel.csv"
+    NER_MODEL_PATH = "./models/ner_chia_test2"
+    RE_MODEL_PATH = "./models/re_test_small_fullDownsampled/checkpoint-52"
+    OUTPUT_DIR = "./results/test"
+    # create output directory
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    
+    
+    # %%
+    # --- (1) load ctg-dataset ---
+    print(f"\nLoading test dataset from {DATA_DIR}...")
+    df = pd.read_csv(DATA_DIR)
+    # test fragment
+    df = df[:2]
+    print(f"Test dataset successfully loaded.")
+    
+    
+    # %%
+    # --- (2) load NER model and tokenizer ---
     print("\nLoading NER model and tokenizer...")
     # - aggregation_strategy="simple"
     # --> merge sub-tokens into words
@@ -150,46 +171,95 @@ def main():
     )
     print("NER model and tokenizer successfully loaded.")
     
-    # (2) load RE model and tokenizer
+    
+    # %%
+    # --- (3) load RE model and tokenizer ---
     print("\nLoading RE model and tokenizer...")
     re_tokenizer = AutoTokenizer.from_pretrained(RE_MODEL_PATH)
     re_model = AutoModelForSequenceClassification.from_pretrained(RE_MODEL_PATH)
     print("RE model and tokenizer successfully loaded.")
-
-    # (3) input
-    example_text = "Pregnant females."
     
-    # (4) apply NER
-    entities = run_ner_module(example_text, ner_pipeline)
-    
-    # (5) apply RE to NER results   
-    relations = run_re_module(example_text, entities, re_tokenizer, re_model)
-    if relations:
-        print(f"RE identified {len(relations)} relations:")
-        for rel in relations:
-            print(f"\tEnt1: {rel["arg1"]}, Ent2: {rel["arg2"]}, Relation: [{rel["relation"]}], Confidence: ({rel["confidence"]:.2f})")
-    else:
-        print("RE found no relations.")
-    
-    
-    # (6) save RE results 
-    re_output_path = os.path.join(OUTPUT_DIR, "re_output.json")
-    with open(re_output_path, "w", encoding="UTF-8") as f:
-        json.dump(relations, f, indent=4)
-    
-    print(f"\nFinal relation extraction results saved to {re_output_path}")
-    
-
-# %%
-if __name__ == "__main__":
     
     # %%
-    # configurations
-    NER_MODEL_PATH = "./models/ner_chia_test2"
-    RE_MODEL_PATH = "./models/re_test_small_fullDownsampled/checkpoint-52"
-    OUTPUT_DIR = "./results/test"
-    # create output directory
-    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    # --- (4) apply NER ---
+    print(f"\nRunning NER inference on all sentences...")
+    entities = list()
+    entities_column = list()
+    for idx, row in df.iterrows():
+        text = row["Sentence"]
+        ner_prediction = run_ner_module(text, ner_pipeline)
+        sentence_entry = {
+            "sentence_id": idx,
+            "text": text,
+            "entities": ner_prediction,
+            "nct_id": row["StudyNCTid"],
+            "criteria_type": row["criteria_type"]
+        }
+        entities.append(sentence_entry)
+        # entities_column.append(json.dumps(ner_prediction))
+        entities_column.append(ner_prediction)
+    # save entities in dataframe
+    df["Entities"] = entities_column
+    print("NER inference on all sentences successfully completed.")
+    
     
     # %%
-    main()
+    # --- (4.1) save NER predictions ---
+    ner_output_path_jsonl = os.path.join(OUTPUT_DIR, "ner_predictions.jsonl")
+    ner_output_path_csv = os.path.join(OUTPUT_DIR, "ner_predictions.csv")
+    # JSONL format
+    with open(ner_output_path_jsonl, "w", encoding="UTF-8") as f:
+        for sent in entities:
+            json.dump(sent, f)
+            f.write("\n")
+    print(f"\nNER predictions saved in JSONL-format to: {ner_output_path_jsonl}")
+    # csv format
+    df.to_csv(ner_output_path_csv, index=False)
+    print(f"\nNER predictions saved in csv-format to: {ner_output_path_csv}")
+    
+
+    # %%
+    # --- (5) apply RE to NER predictions ---
+    relations = list()
+    relations_column = list()
+    for idx, row in df.iterrows():
+        text = row["Sentence"]
+        entities = row["Entities"]
+        re_prediction = run_re_module(text, entities, re_tokenizer, re_model)
+        sentence_entry = {
+            "sentence_id": idx,
+            "text": text,
+            "relations": re_prediction,
+            "nct_id": row["StudyNCTid"],
+            "criteria_type": row["criteria_type"]
+        }
+        relations.append(sentence_entry)
+        relations_column.append(re_prediction)
+
+        # print()
+        # print(f"Sentence: {text}")
+        # print(f"Predicted relations:")
+        # for rel in re_prediction:
+        #     print(f"\t{rel["relation"]}({rel["arg1_type"]}({rel["arg1"]}), {rel["arg2_type"]}({rel["arg2"]}))")
+    
+    # save relations in dataframe
+    df["Relations"] = relations_column
+    print("RE inference on all sentences successfully completed.")
+        
+            
+    # %%
+    # --- (5.1) save RE results ---
+    re_output_path_jsonl = os.path.join(OUTPUT_DIR, "re_predictions.jsonl")
+    re_output_path_csv = os.path.join(OUTPUT_DIR, "re_predictions.csv")
+    # JSONL format
+    with open(re_output_path_jsonl, "w", encoding="UTF-8") as f:
+        for sent in relations:
+            json.dump(sent, f)
+            f.write("\n")
+    print(f"\nRE predictions saved in JSONL-format to: {re_output_path_jsonl}")
+    # csv format
+    df.to_csv(re_output_path_csv, index=False)
+    print(f"\nRE predictions saved in csv-format to: {re_output_path_csv}")
+
+    print(f"\nNER and RE inference pipeline successfully completed.")
+    print(f"Results can be found at {OUTPUT_DIR}")
