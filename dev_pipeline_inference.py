2bd52a9364c04ff0d5c2882cb647f5a2124d84bf
guetmatt <mguetebier@uni-osnabrueck.de>
2026-01-21 16:58:57 +0100

    new pipeline inference script

--- dev_pipeline_inference.py
+++ dev_pipeline_inference.py
@@ -1,195 +1,195 @@
-# 3. Next Step: Batch Processing for CTG For the actual CTG dataset processing, do not loop one by one as shown in the "TEST" block. Instead:
-# Run NER on the whole CTG dataset (batches).
-# Store all extracted entities in a JSON file (intermediate storage).
-# Load the JSON, generate pairs, and run RE in batches.
+
+# to do
+# - save ner results - eigene function
+# - adjust ner results to jsonl, andere inhalte etc
+# - save re results - eigene function
+# - add full text/sentence to ner results
 
 
-# TO DO
-# - load NER result-files for RE
-# - store RE results as file
-# - work on RE trainng
-# - work on NER training
- 
 
 # %%
 # imports
+import os
+import json
 import torch
 import itertools
-from transformers import AutoTokenizer, AutoModelForTokenClassification, AutoModelForSequenceClassification
+from transformers import pipeline, AutoTokenizer, AutoModelForSequenceClassification
 
 
 # %%
 # functions
-def extract_entities_from_ner(text, tokenizer, model):
+def run_ner_module(text, ner_pipe):
     """
-    Run NER, aggregate BIO tags to character-level entity spans for RE.
-    Returns:
-        - list of dicts {"entity": label, "start": char_idx, "end": char_idx, "text": str}
+    Applies NER model to text.
+    Returns list of entities and saves them to disk.
     """
-
-    inputs = tokenizer(text, return_tensors="pt", return_offsets_mapping=True)
-    offset_mapping = inputs["offset_mapping"][0].tolist()
+    # apply NER
+    entities = ner_pipe(text)
     
-    with torch.no_grad():
-        logits = model(**{k: v for k, v in inputs.items() if k != "offset_mapping"}).logits
-
-    predictions = torch.argmax(logits, dim=2)[0].tolist()
-    id2label = model.config.id2label
+    # maybe --> filter low confidence entities
+    # entities = [ent for ent in entities if ent['score'] > 0.4
     
-    entities = list()
-    current_entity = None
+    # save results
+    ner_output_path = os.path.join(OUTPUT_DIR, "ner_output.json")
+    with open(ner_output_path, "w", encoding="UTF-8") as f:
+        serializable_entities = list()
+        for ent in entities:
+            ent_clean = ent.copy()
+            ent_clean["score"] = float(ent["score"])
+            ent_clean["start"] = int(ent["start"])
+            ent_clean["end"] = int(ent["end"])
+            serializable_entities.append(ent_clean)
+        json.dump(serializable_entities, f, indent=4)
+    print(f"NER results saved to {ner_output_path}")
     
-    for idx, pred_id in enumerate(predictions):
-        label = id2label[pred_id]
-        start_char, end_char = offset_mapping[idx]
-        
-        # ignore special tokens ([CLS], [PAD], etc.)
-        if start_char == end_char == 0:
-            continue
-        
-        # beginning of an entity
-        if label.startswith("B-"):
-            if current_entity:
-                entities.append(current_entity)
-            current_entity = {
-                "label": label[2:], # remove "B-"
-                "start": start_char,
-                "end": end_char,
-                "tokens": [idx]
-            }
-        # inside of an entity
-        elif label.startswith("I-") and current_entity:
-            # label consistent with previous entity label
-            if label[2:] == current_entity["label"]:
-                current_entity["end"] = end_char
-                current_entity["tokens"].append(idx)
-            # label not consistent with previous entity label
-            else:
-                entities.append(current_entity)
-                current_entity = None
-        
-        # label: no entity (after last token of current entity)
-        else:
-            if current_entity:
-                entities.append(current_entity)
-                current_entity = None
-        
-    if current_entity:
-        entities.append(current_entity)
-    
-    # extract text of entities
-    # for verification
-    for ent in entities:
-        ent["text"] = text[ent["start"]:ent["end"]]
-    
-    return entities
+    return serializable_entities
 
 
 
-def format_re_input(text, entity_1, entity_2):
+def format_re_input(text, entity1, entity2):
     """
-    Injects [E1]...[/E1] and [E2]...[/E2] markers into the text.
-    Handles overlapping entities by prioritizing strict injection order.
+    Injects markers [E1]...[/E1] and [E2]...[/E2] for the RE model.
+    Inserts from right-to-left to prevent index shifting.
     """
-    
-    # sort by start index
-    # -> handle insertion correctly
-    # insert the later entity first
-    # -> indices of earlier entity dont shift
-    e1_start, e1_end = entity_1["start"], entity_1["end"]
-    e2_start, e2_end = entity_2["start"], entity_2["end"]
-    
+    e1_start, e1_end = entity1["start"], entity1["end"]
+    e2_start, e2_end = entity2["start"], entity2["end"]
     insertions = [
         (e1_start, "[E1]"), (e1_end, "[/E1]"),
         (e2_start, "[E2]"), (e2_end, "[/E2]")
     ]
     
-    # reverse = position descending (to insert later entities first)
+    # sort descending by position
+    # --> to insert markers back to front
     insertions.sort(key=lambda x: x[0], reverse=True)
-    
-    
+
     marked_text = text
     for pos, marker in insertions:
+        if pos > len(marked_text):
+            continue
         marked_text = marked_text[:pos] + marker + marked_text[pos:]
-        
+    
     return marked_text
 
 
 
-def predict_relation(text, tokenizer, model):
-    inputs = tokenizer(text, return_tensors="pt", padding=True, truncation=True, max_length=MAX_LEN)
-    with torch.no_grad():
-        logits = model(**inputs).logits
-    pred_id = torch.argmax(logits, dim=1).item()
-    
-    return model.config.id2label[pred_id]
-
-
-
-# main pipeline
-def process_sentence(sentence):
-    print(f"\nOriginal sentence: {sentence}")
-    
-    # NER
-    entities = extract_entities_from_ner(sentence, ner_tokenizer, ner_model)
-    entities_and_labels = [ent["text"] + " (" + ent["label"] + ")" for ent in entities]
-    print(f"Found {len(entities)} entities: {entities_and_labels}")
+def run_re_module(text, entities, re_tokenizer, re_model):
+    """
+    Takes text and entities (from NER), generates pairs, and predicts relations.
+    """
     
     if len(entities) < 2:
-        print("Not enough entities for relation extraction. Need at least two entities.")
-        return None
-    
-    # candidate pair generation for RE
-    # permutations -> implies directionality of relations matters
+        print("Need at least 2 entities for relation extraction.")
+        return []
+
+    # generate permutations
+    # entity pairs for possible relations
     pairs = list(itertools.permutations(entities, 2))
+    found_relations = list()
     
-    print(f"Testing {len(pairs)} candidate pairs for RE...")
     for e1, e2 in pairs:
-        # maker injection
-        try:
-            marked_text = format_re_input(sentence, e1, e2)
         # skip overlapping entities
-        except Exception as e:
-            print(f"Skipping pair {e1, e2} due to overlap/error: {e}")
+        if max(e1["start"], e2["start"]) < min(e1["end"], e2["end"]):
             continue
         
-        # RE
-        relation = predict_relation(marked_text, re_tokenizer, re_model)
+        # entity marker injection
+        try:
+            marked_text = format_re_input(text, e1, e2)
+        except Exception as e:
+            print(f"Warning: formatting error pair {e1["word"]} / {e2["word"]}: {e}")
+            continue
         
-        # relation found
-        if relation != "NO_RELATION":
-            print(f"RELATION FOUND: {e1["text"]} -> {relation} -> {e2["text"]}") 
-            print(f"\tContext: {marked_text}")
-        else:
-            print(f"No relation found.")
+        # tokenization
+        inputs = re_tokenizer(
+            marked_text,
+            return_tensors="pt",
+            truncation=True,
+            max_length=256,
+            padding="max_length"
+        )
+        
+        # relation prediction
+        re_model.eval()
+        with torch.no_grad():
+            outputs = re_model(**inputs)
+        logits = outputs.logits
+        pred_id = torch.argmax(logits, dim=1).item()
+        pred_label = re_model.config.id2label[pred_id]
+        confidence = torch.softmax(logits, dim=1)[0][pred_id].item()
+        
+        # save predicted relations
+        if pred_label != "NO_RELATION":
+            found_relations.append({
+                "arg1": e1["word"],
+                "arg1_type": e1["entity_group"],
+                "arg2": e2["word"],
+                "arg2_type": e2["entity_group"],
+                "relation": pred_label,
+                "confidence": confidence,
+                "context": marked_text
+            })
     
-           
-
-# %%
-# testing
-if __name__ == "__main__":
+    return found_relations
     
-    # configuration
-    NER_MODEL_PATH = "./models/ner_chia_test2"
-    RE_MODEL_PATH = "./models/bert-re-v1"
-    MAX_LEN = 256
+    
+    
+    
 
-    # %%
-    # load models
-    print("Loading models...")
-
-    ner_tokenizer = AutoTokenizer.from_pretrained(NER_MODEL_PATH)
-    ner_model = AutoModelForTokenClassification.from_pretrained(NER_MODEL_PATH)
-
+def main():
+    # (1) load NER model and tokenizer
+    print("\nLoading NER model and tokenizer...")
+    # - aggregation_strategy="simple"
+    # --> merge sub-tokens into words
+    # - device=-1
+    # --> -1=CPU, 0=GPU
+    ner_pipeline = pipeline(
+        "token-classification",
+        model=NER_MODEL_PATH,
+        tokenizer=NER_MODEL_PATH,
+        aggregation_strategy="simple",
+        device=-1
+    )
+    print("NER model and tokenizer successfully loaded.")
+    
+    # (2) load RE model and tokenizer
+    print("\nLoading RE model and tokenizer...")
     re_tokenizer = AutoTokenizer.from_pretrained(RE_MODEL_PATH)
     re_model = AutoModelForSequenceClassification.from_pretrained(RE_MODEL_PATH)
+    print("RE model and tokenizer successfully loaded.")
 
-    # %%
-    # models in eval-mode (ignores some training-specific layers)
-    ner_model.eval()
-    re_model.eval()
+    # (3) input
+    example_text = "Pregnant females."
+    
+    # (4) apply NER
+    entities = run_ner_module(example_text, ner_pipeline)
+    
+    # (5) apply RE to NER results   
+    relations = run_re_module(example_text, entities, re_tokenizer, re_model)
+    if relations:
+        print(f"RE identified {len(relations)} relations:")
+        for rel in relations:
+            print(f"\tEnt1: {rel["arg1"]}, Ent2: {rel["arg2"]}, Relation: [{rel["relation"]}], Confidence: ({rel["confidence"]:.2f})")
+    else:
+        print("RE found no relations.")
+    
+    
+    # (6) save RE results 
+    re_output_path = os.path.join(OUTPUT_DIR, "re_output.json")
+    with open(re_output_path, "w", encoding="UTF-8") as f:
+        json.dump(relations, f, indent=4)
+    
+    print(f"\nFinal relation extraction results saved to {re_output_path}")
+    
+
+# %%
+if __name__ == "__main__":
     
     # %%
-    # example sentence
-    test_sentence = "Pregnant female."
-    process_sentence(test_sentence)
+    # configurations
+    NER_MODEL_PATH = "./models/ner_chia_test2"
+    RE_MODEL_PATH = "./models/re_test_small_fullDownsampled/checkpoint-52"
+    OUTPUT_DIR = "./results/test"
+    # create output directory
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+    
+    # %%
+    main()
